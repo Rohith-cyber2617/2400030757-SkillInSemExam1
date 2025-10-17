3570293ac7b340fc49c3ae85c466a0ca6656ad2a
Tony <rohithrachapudi96@gmail.com>
2025-10-17 10:14:54 +0530

    Add App.js for main branch

---
+++ App.js
@@ -0,0 +1,10 @@
+function greetUser(name) {
+  console.log("Welcome, " + name + "!");
+}
+
+function appStatus() {
+  console.log("Main branch: Application running smoothly.");
+}
+
+greetUser("Developer");
+appStatus();
