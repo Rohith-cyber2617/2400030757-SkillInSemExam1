7193264cd877b691157b6ada8765a1765380b77f
Tony <rohithrachapudi96@gmail.com>
2025-10-17 11:04:01 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
 function greetUser(name) {
   console.log("Hi " + name + ", Bob improved the backend!");
-}
-
-function appStatus() {
-  console.log("Feature2: Backend optimization completed.");
-}
-
-greetUser("Bob");
   console.log("Hello " + name + ", this is Alice’s version!");
 }
 
 function appStatus() {
   console.log("Feature1: UI changes added.");
+  console.log("Feature2: Backend optimization completed.");
 }
 
 greetUser("Alice");
